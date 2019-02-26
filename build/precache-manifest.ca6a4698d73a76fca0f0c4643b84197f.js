d313ab278ee91f739936985c892d3d580c74b734
EC2 Default User <ec2-user@ip-172-31-92-210.ec2.internal>
2019-02-26 02:58:29 +0000

    initial commit

---
+++ build/precache-manifest.ca6a4698d73a76fca0f0c4643b84197f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5078a14a2ade7d3b2164",
+    "url": "/static/js/main.5078a14a.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5078a14a2ade7d3b2164",
+    "url": "/static/css/main.7132b0c7.chunk.css"
+  },
+  {
+    "revision": "bdd980193f9a0b21b54fc6b8c4e16fd8",
+    "url": "/index.html"
+  }
+];
